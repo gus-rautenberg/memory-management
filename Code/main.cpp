71a2296aec259fc2d304edc17d12e9df74a33b61
Joao-Biederman <joaobiedetman@gmail.com>
2024-02-22 18:28:56 -0300

    teste threads deu erro

--- Code/main.cpp
+++ Code/main.cpp
@@ -15,7 +15,6 @@ sem_t fullOut, emptyOut, mutexOut;
 
 struct thread_args
 {
-    OPT memory;
     vector<int> reference_string;
     int i;
     int j;
@@ -26,8 +25,8 @@ ofstream out("results.out");
 void *run_opt(void *threadarg){
     thread_args *args;
     args = (thread_args *) threadarg;
-
-    int miss = args->memory.count_misses(args->reference_string);
+    OPT memory(args->j);
+    int miss = memory.count_misses(args->reference_string);
     sem_wait(&emptyOut);
     sem_wait(&mutexOut);
     out << "OPT Misses from trace" << args->i << " with " << args->j << " pages: " << miss << endl;
@@ -43,25 +42,32 @@ int main(int argc, char const *argv[])
     sem_init(&emptyOut, 0, 12);
     sem_init(&mutexOut, 0, 1);
 
-    pthread_t thread[5][33];
+    pthread_t thread[16];
+    int teste = 0;
 
     vector<int> reference_string[5];
-    for (size_t i = 1; i <= 4; i++) {
+    for (int i = 1; i <= 4; i++) {
         vector<string> log = getLog(to_string(i));
         reference_string[i] = generateReferenceString(log);
 
+        cout << "foi o log" << endl;
+
         for (int j = 4; j <= 32; j *= 2) {
-            out << "LRU Misses: " << lruAlgorithm(reference_string[i], j);
-            OPT memory(j);
+            int miss = lruAlgorithm(reference_string[i], j);
+            cout << "LRU Misses: " << miss << endl;
             thread_args args;
-            args.memory = memory;
             args.reference_string = reference_string[i];
             args.i = i;
+            cout << i << j << endl;
             args.j = j;
 
-            pthread_create(&thread[i-1][j-1], NULL, &run_opt, NULL);
-            pthread_join(thread[i-1][j-1], NULL);
+            pthread_create(&thread[teste], NULL, &run_opt, (void *) &args);
+            teste++;
         }   
+        
     }
+    for (int i = 0; i < 16; i++) {
+        pthread_join(thread[i], NULL);
+    }    
     return 0;
 }
